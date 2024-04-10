3dc84578ebab7494701279eb5dfe48629681a290
Baiheng Xie <874256269@qq.com>
2024-04-10 10:59:57 +0800

    refact: simpleJWT+redis重构账号模块

--- oj/account/urls.py
+++ oj/account/urls.py
@@ -1,11 +1,27 @@
 from django.urls import path
-from .views import UserDetailAPI, RegisterUserAPIView, LogoutAPIView, UserProfileAPIView
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+from .views import (UserDetailAPI,
+                    RegisterUserAPI,
+                    LoginAPI,
+                    LogoutAPI,
+                    UserProfileAPI,
+                    EditUserProfileAPI)
+
 
 urlpatterns = [
+    path('register', RegisterUserAPI.as_view(), name='register'),
+    path('logout', LogoutAPI.as_view(), name='logout'),
+    path('login', LoginAPI.as_view(), name='login'),
+    path('profile/<str:username>', UserProfileAPI.as_view(), name='user-profile'),
+    path('profile', EditUserProfileAPI.as_view(), name='edit-user-profile'),
+    # !test api
     path("user/<int:pk>", UserDetailAPI.as_view()),
-    path('register', RegisterUserAPIView.as_view()),
-    path('logout', LogoutAPIView.as_view()),
-    path('profile/<int:user_id>', UserProfileAPIView.as_view(), name='user-profile'),
-    path('login', obtain_auth_token),
+    # !used for token test
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
---
+++ oj/utils/token.py
@@ -0,0 +1,18 @@
+import jwt
+import time
+
+from oj import settings
+
+
+def get_token_info(token, para='user_id'):
+    """
+        解析 jwt 得到 payload 里某一字段, 默认为 user_id
+    """
+    try:
+        decode_data = jwt.decode(
+            token, secret_key=settings.SECRET_KEY, verify=False, algorithms=['HS256'])
+        if int(decode_data['exp']) < int(time.time()):
+            return "token expired"
+        return decode_data['user_id']
+    except Exception as e:
+        return "token error:\n" + str(e)
