9ce0fe682c14ed9622635f033e63e083d9e10a3f
Baiheng Xie <874256269@qq.com>
2024-05-12 21:34:41 +0800

    feat: basic apis and models

--- oj/discussion/views.py
+++ oj/discussion/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from utils.api import APIView
+from utils.token import JWTAuthTokenSerializer
+
+
+class PostViewAPI(APIView):
+
+    def get(self, request, id):
+        pass
+
+
+class PostUserAPI(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = JWTAuthTokenSerializer
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
+
+
+class CommentViewAPI(APIView):
+    '''
+        Given post_id, return all comments of the post.
+    '''
+
+    def get(self, request):
+        pass
+
+
+class CommentUserAPI(APIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = JWTAuthTokenSerializer
+
+    def post(self, request):
+        pass
